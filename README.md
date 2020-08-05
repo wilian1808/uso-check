8581663017013c37ce5dda03ea8a3d66a4eb89f4
wilian1808 <wilian.walli@gmail.com>
2020-08-05 13:32:51 -0500

    first

--- README.md
+++ README.md
@@ -21,3 +21,32 @@ considerando la siguiente base de datos relacional.
 
 | JefeID | Nombre | SueldoJefe |
 | ------ | ------ | ---------- |
+
+
+### 2. Ejercicio adicional.
+
+En su tabla `Estudiantes` del trabajo anterior, agregar el campo para la imagen del estudiante, insertar su imagen.
+
+Podemos encontrar la tabla en el siguiente enlace:
+
+- [Tabla estudiantes](https://github.com/wilian1808/restricciones-integridad#1-primero)
+
+> Para poder insertar una imagen en la base de dato se agrego una nueva columna llamada *Imagen* de formato texto, se escogio este formato ya que las imagenes se guardan en servicios externos y lo unico que se inserta en las bases de datos son las urls de donde se encuentra almacenada nuestra imagen. Como estamos alterando una tabla ya existente para que los estudiantes no esten con una imagen en blanco definimos un valor por defecto.
+
+```sql
+ALTER TABLE Estudiantes 
+    ADD COLUMN Imagen TEXT NOT NULL 
+    DEFAULT 'https://www.rastreator.com/wp-content/uploads/106-300x300.jpg'
+```
+
+> Ahora procedemos a actualizar los valores de la imagen para cada estudiante segundo correspondan su id.
+
+```sql
+UPDATE Estudiantes 
+    SET Imagen = 'https://i.pinimg.com/474x/b0/33/6d/b0336de8b6ca431d44e0ad5ed76b1dd8.jpg'
+    WHERE EstudianteID = 1;
+
+UPDATE Estudiantes 
+    SET Imagen = 'https://i.pinimg.com/474x/b0/33/6d/b0336de8b6ca431d44e0ad5ed76b1dd8.jpg'
+    WHERE EstudianteID = 2;
+```
--- file.sql
+++ file.sql
@@ -1,7 +1,3 @@
-
-
-
-
 CREATE TABLE empresas (
     id_empresa INT NOT NULL,
     nombre_empresa VARCHAR(100) NOT NULL,
@@ -9,13 +5,6 @@ CREATE TABLE empresas (
     CONSTRAINT pk_empresas PRIMARY KEY (id_empresa),
     CONSTRAINT uk_empresas UNIQUE (nombre_empresa)
 );
-GO
-
-INSERT INTO empresas VALUES (1, 'huellitas', 'arequipa'),
-                            (2, 'mibanco', 'puno'),
-                            (3, 'agrotour', 'lima'),
-                            (4, 'kingmotors', 'juliaca');
-GO
 
 CREATE TABLE empleados (
     id_empleado INT NOT NULL,
@@ -26,14 +15,6 @@ CREATE TABLE empleados (
     CONSTRAINT pk_empleados PRIMARY KEY (id_empleado),
     CONSTRAINT ck_empleados CHECK (sueldo_empleado > 0)
 );
-GO
-
-INSERT INTO empleados VALUES (1, 'leon', 'av. torres', 'lima', 920.50),
-                             (2, 'wilber', 'jr. lima', 'arequipa', 1050.60),
-                             (3, 'hebert', 'av. torres', 'puno', 920.50),
-                             (4, 'mario', 'jr. inca', 'juliaca', 1050.60),
-                             (5, 'mary', 'av. floral', 'tacna', 950.20);
-GO
 
 CREATE TABLE jefes (
     id_jefe INT NOT NULL,
@@ -42,16 +23,6 @@ CREATE TABLE jefes (
     CONSTRAINT pk_jefes PRIMARY KEY (id_jefe),
     CONSTRAINT ck_jefes CHECK (sueldo_jefe > 0)
 );
-GO
-
-INSERT INTO jefes VALUES (1, 'oscar', 1800.20),
-                         (2, 'jose', 950.80);
-GO
-
--- creamos una tabla que nos ayudara a la relacion de:
--- un jefe puede tener varios empleados
--- un empleado solo puede tener un jefe
--- el sueldo del empleado no puede ser superior al del jefe
 
 CREATE TABLE jf (
     id_je INT NOT NULL,
@@ -67,19 +38,6 @@ CREATE TABLE jf (
     -- comprueba que el sueldo del jefe es mayor al empleado
     CONSTRAINT ck_jf CHECK (sueldo_jefe > sueldo_empleado)
 );
-GO
-
-INSERT INTO jf VALUES (1, 1, 1, 1800.20, 920.50),
-                      (2, 2, 3, 950.80, 920.50),
-                      (3, 2, 5, 950.80, 950.20),
-                      (4, 1, 2, 1800.20, 1050.60),
-                      (5, 1, 4, 1800.20, 1050.60);
-GO
-
--- tabla que nos ayudara a la relacion de:
--- un empleado trabaja en una sola empresa
--- una empresa pueden tener varios empleados
--- cada empleado trabaje para una empresa ubicada en su ciudad de residencia
 
 CREATE TABLE trabajos (
     id_trabajo INT NOT NULL,
@@ -96,21 +54,10 @@ CREATE TABLE trabajos (
     CONSTRAINT ck_trabajos CHECK (ciudad_empleado = ciudad_empresa)
 );
 
-INSERT INTO trabajos VALUES (1, 1, 'agrotour', 'lima', 'lima'),
-                            (2, 2, 'huellitas', 'arequipa', 'arequipa'),
-                            (3, 3, 'mibanco', 'puno', 'puno'),
-                            (4, 4, 'kingmotors', 'juliaca', 'juliaca');
-GO
-
--- NOTA: si intentamos agregar datos donde no se cumplan las restricciones 'check'
--- nos devolvera un error, actualmente estoy probando el codigo en un servicio
--- llamado: http://sqlfiddle.com/ donde puedo ejecutar codigo de sql - server
--- las capturas se realizaron de esta pagina - tuve problemas al instalar
--- sql server en windows
 
 -- EJERCICIO ADICIONAL:
 -- en su tabla estudiante del trabajo anterior, agregar el campo para imagen del
--- estudiante. Luego insertar una imagen
+-- estudiante. Lu insertar una imagen
 
 CREATE TABLE estudiantes (
     id_estudiante INT NOT NULL,
@@ -128,13 +75,7 @@ CREATE TABLE estudiantes (
     CONSTRAINT ck_dni_estudiantes CHECK (dni > 0),
     CONSTRAINT uk_correo_estudiantes UNIQUE (correo)
 );
-GO
 
 INSERT INTO estudiantes VALUES 
     (1, 'leon', 'chipana', 174547, 12457885, 'leon@gmail.com', 'M', 'https://static.thenounproject.com/png/446003-200.png'),
     (2, 'heber', 'charca', 154544, 78855412, 'hebert@gmail.com', 'M', 'https://static.thenounproject.com/png/446003-200.png');
-GO
-
--- se escogio el formato text para la columna de imagen por que generalmente
--- se almacena la imagen en un servicio externo y solamente se guarda la url
--- en la base de datos
